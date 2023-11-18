200ed54bb7c69c2c2f2a186cdfe6b23bee977d02
jodydev <jodyossino2001@gmail.com>
2023-11-18 11:30:22 +0100

    fix

--- src/components/Sponsor/SponsorBannerPayPal.jsx
+++ src/components/Sponsor/SponsorBannerPayPal.jsx
@@ -5,12 +5,12 @@
                 <div className="row">
                   <div className="col-6 transition">
                     <div className="container rounded-5 w-100 shadow-lg d-flex justify-content-center slide-in-left transition">
-                      <img src="https://www.ajonoas.it/wp-content/uploads/2021/01/PayPal-Logo.png " height="170"  alt="PayPal" />
+                      <img src="https://www.ajonoas.it/wp-content/uploads/2021/01/PayPal-Logo.png" height="170"  alt="PayPal" />
                     </div>
                   </div>
                   <div className="col-6">
                     <div className="container slide-in-right transition">
-                    <img src="/src/assets/img/sponsor/paypal-info.png" height="170" className="rounded-5 w-100 shadow-lg transition" alt="PayPal Info" />
+                    <img src="/assets/img/sponsor/paypal-info.png" height="170" className="rounded-5 w-100 shadow-lg transition" alt="PayPal Info" />
                     </div>
                   </div>
                 </div>
